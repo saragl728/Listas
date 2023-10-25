152850a477dd6ca191d574636d44969e9c95e730
Usuario <saragl728@gmail.com>
2023-10-25 08:41:53 +0200

    Sobreescribo el método getItemCount

--- app/src/main/java/com/example/listas/adapter/FilmAdapter.kt
+++ app/src/main/java/com/example/listas/adapter/FilmAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.listas.adapter
-
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listas.Film
 
-class FilmAdapter : RecyclerView.Adapter<FilmViewHolder> {
+class FilmAdapter(val filmList: List<Film>) : RecyclerView.Adapter<FilmViewHolder> {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
         TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return filmList.size
     }
 
     override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
