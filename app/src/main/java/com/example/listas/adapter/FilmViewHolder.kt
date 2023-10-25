2909b2dd65539870e0164f8f506828af542e063f
Usuario <saragl728@gmail.com>
2023-10-25 09:07:20 +0200

    Implemento la clase FilmViewHolder

--- app/src/main/java/com/example/listas/adapter/FilmViewHolder.kt
+++ app/src/main/java/com/example/listas/adapter/FilmViewHolder.kt
@@ -1,4 +1,15 @@
 package com.example.listas.adapter
 
-class FilmViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.listas.Film
+import com.example.listas.databinding.ItemFilmBinding
+
+class FilmViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val binding = ItemFilmBinding.bind(view)
+    fun render(filmModel: Film){
+        binding.FilmName.text = filmModel.title
+        binding.FilmDirector.text = filmModel.director
+        binding.FilmYear.text = filmModel.year.toString()
+    }
 }
