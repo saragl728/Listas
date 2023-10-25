698787f2b960d86e701d8530a0a71129de707605
Usuario <saragl728@gmail.com>
2023-10-25 10:00:23 +0200

    Hago que acceda a las imágenes y doy permiso para acceder a internet

--- app/src/main/java/com/example/listas/adapter/FilmViewHolder.kt
+++ app/src/main/java/com/example/listas/adapter/FilmViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.listas.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.listas.Film
 import com.example.listas.databinding.ItemFilmBinding
 
@@ -11,5 +12,6 @@ class FilmViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         binding.FilmName.text = filmModel.title
         binding.FilmDirector.text = filmModel.director
         binding.FilmYear.text = filmModel.year.toString()
+        Glide.with(binding.FilmPhoto.context).load(filmModel.foto).into(binding.FilmPhoto)
     }
 }
