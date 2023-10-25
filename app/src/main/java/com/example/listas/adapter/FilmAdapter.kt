e82867744e724e2d7e4051cdde3322acf78eda3d
Usuario <saragl728@gmail.com>
2023-10-25 08:37:14 +0200

    Hago que FilmAdapter herede de una clase e implemento los métodos

--- app/src/main/java/com/example/listas/adapter/FilmAdapter.kt
+++ app/src/main/java/com/example/listas/adapter/FilmAdapter.kt
@@ -1,4 +1,18 @@
 package com.example.listas.adapter
 
-class FilmAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class FilmAdapter : RecyclerView.Adapter<FilmViewHolder> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
