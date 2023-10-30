aeb2d7cc49b813abad523ba8b5939310b82e2b8d
Usuario <saragl728@gmail.com>
2023-10-30 09:46:56 +0100

    Añado líneas divisorias

--- app/src/main/java/com/example/listas/MainActivity.kt
+++ app/src/main/java/com/example/listas/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.listas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listas.adapter.FilmAdapter
 import com.example.listas.databinding.ActivityMainBinding
@@ -10,10 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        val decoration = DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recycler.layoutManager = LinearLayoutManager(this)
         binding.recycler.adapter = FilmAdapter(FilmProvider.filmList)
+        binding.recycler.addItemDecoration(decoration)
     }
 }
