66a4588803e0c804627d04d480d962a4ebe6e29b
Usuario <saragl728@gmail.com>
2023-10-30 09:34:43 +0100

    Hago que se muestre una tostada si se hace click sobre una imagen

--- app/src/main/java/com/example/listas/adapter/FilmViewHolder.kt
+++ app/src/main/java/com/example/listas/adapter/FilmViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.listas.adapter
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.listas.Film
@@ -13,5 +14,8 @@ class FilmViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         binding.FilmDirector.text = filmModel.director
         binding.FilmYear.text = filmModel.year.toString()
         Glide.with(binding.FilmPhoto.context).load(filmModel.foto).into(binding.FilmPhoto)
+        binding.FilmPhoto.setOnClickListener {
+            Toast.makeText(binding.FilmPhoto.context, filmModel.title, Toast.LENGTH_LONG).show()
+        }
     }
 }
